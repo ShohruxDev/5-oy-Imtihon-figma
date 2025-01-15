14fe1a163f9acb0a03d9cb982cfbe631eda0aba3
Timurbek <timurbeksaburovv.@gmail.com>
2025-01-15 11:04:01 +0200

    5-oy-I-F-si

---
+++ app.js
@@ -0,0 +1,49 @@
+
+const cartCount = document.querySelector('.count');
+const plusBtn = document.querySelector('.plus-btn');
+// var count = localStorage.getItem('cartCount') ?? 0;
+let count = 0
+plusBtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    count+=1;
+    cartCount.textContent = count;
+    localStorage.setItem('cartCount',count)
+})
+
+const toggleButton1 = document.getElementById('toggleButton1');
+const toggleButton2 = document.getElementById('toggleButton2');
+const section2 = document.getElementById('section2');
+
+toggleButton1.addEventListener('click', () => {
+    section2.classList.remove('default-none'); 
+    toggleButton2.style.display = 'block'; 
+});
+
+toggleButton2.addEventListener('click', () => {
+    section2.classList.add('default-none'); 
+    toggleButton2.style.display = 'none'; 
+});
+const backToTopButton = document.getElementById("backToTop");
+window.onscroll = function () {
+    scrollFunction();
+};
+function scrollFunction() {
+    if(
+        document.body.scrollTop > 200 ||
+        document.documentElement.scrollTop > 200
+    ) {backToTopButton.classList.add("show");
+    } else {
+        backToTopButton.classList.remove("show");
+    }
+
+    
+}
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    })
+}
+backToTopButton.addEventListener("click", scrollToTop);
+
+
